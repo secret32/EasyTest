724d182957195a6fe0601d46087e7302e5c262e0
Secret <secret32@sina.com>
2019-01-15 07:46:43 +0800

    add log service for testing mq

---
+++ LogService/src/main/java/org/mytest/springcloud/LogServerApplication.java
@@ -0,0 +1,61 @@
+package org.mytest.springcloud;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConversionException;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhouwei
+ * @since 1/14/2019
+ */
+@EnableRabbit
+@EnableDiscoveryClient
+@SpringBootApplication
+@Configuration
+public class LogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogServerApplication.class);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new MessageConverter() {
+            @Override
+            public Message toMessage(Object object, MessageProperties messageProperties) throws MessageConversionException {
+                return null;
+            }
+
+            @Override
+            public Object fromMessage(Message message) throws MessageConversionException {
+                return new String(message.getBody());
+            }
+        };
+    }
+
+}
